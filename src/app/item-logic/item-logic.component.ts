d3ea8242c978eccc05670ab054ce44d71642c70b
Evan Taylor <etaylor8086@gmail.com>
2018-12-14 00:30:45 -0800

    Use dropdowns for suitless Magmoor etank counts

--- src/app/item-logic/item-logic.component.ts
+++ src/app/item-logic/item-logic.component.ts
@@ -8,10 +8,16 @@ import { FormGroup } from '@angular/forms';
 })
 export class ItemLogicComponent implements OnInit {
   @Input('group') settingsForm: FormGroup;
+  vmrEtanks = this.numberRange(3, 14);
+  earlyMagmoorEtanks = this.numberRange(7, 14);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private numberRange(start: number, end: number) {
+    return Array.from({ length: end - start + 1 }, (x, i) => start + i);
   }
 
 }
