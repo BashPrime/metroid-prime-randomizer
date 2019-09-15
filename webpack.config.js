69c34ff0a42c9863feb6b9fbe950407933557ca3
Evan Taylor <etaylor8086@gmail.com>
2019-09-15 14:57:45 -0700

    Get vscode debugging working again

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ module.exports = {
   mode: 'production',
   target: 'electron-main',
   entry: './src/electron/main.ts',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]'
   },
   resolve: {
     extensions: ['.ts', '.js']
