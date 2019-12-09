fe2942888ef8dcc5afb87576615c78e130867065
Evan Taylor <etaylor8086@gmail.com>
2019-12-08 23:45:23 -0800

    Presets and modal checkpoint

--- src/client/src/app/generate-game/generate-game.component.ts
+++ src/client/src/app/generate-game/generate-game.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as presetsDefaultJson from '../../assets/data/presetsDefault.json';
+
 @Component({
   selector: 'app-generate-game',
   templateUrl: './generate-game.component.html',
   styleUrls: ['./generate-game.component.scss']
 })
 export class GenerateGameComponent implements OnInit {
+  objectKeys = Object.keys;
+  private defaultPresets: object = presetsDefaultJson;
+  private presets: object = {};
+  private selectedPreset: string = 'Custom';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getDefaultPresets(): object {
+    return this.defaultPresets;
+  }
+
+  getSelectedPreset(): string {
+    return this.selectedPreset;
+  }
+
+  setSelectedPreset(preset: string) {
+    this.selectedPreset = preset;
+  }
 }
