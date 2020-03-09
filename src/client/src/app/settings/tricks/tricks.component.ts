60aeac26fec91ebc96ede2a32311ae7b54100c95
Evan Taylor <etaylor8086@gmail.com>
2020-03-09 01:02:00 -0700

    Checkpoint on setting tricks by difficulty

--- src/client/src/app/settings/tricks/tricks.component.ts
+++ src/client/src/app/settings/tricks/tricks.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, ControlContainer } from '@angular/forms';
+import { FormArray, ControlContainer, FormBuilder } from '@angular/forms';
 
 import { RandomizerService } from '../../services/randomizer.service';
-import { PicklistFormComponent } from 'src/app/components/common/picklist-form.component';
+import { PicklistFormComponent } from '../../components/common/picklist-form.component';
+import { Difficulty } from '../../../../../common/data/settingsDetails';
+
+interface TrickItem {
+  label: string;
+  value: string;
+  tooltip: string;
+  difficulty: string;
+}
 
 @Component({
   selector: 'app-tricks',
@@ -10,15 +18,57 @@ import { PicklistFormComponent } from 'src/app/components/common/picklist-form.c
   styleUrls: ['./tricks.component.scss']
 })
 export class TricksComponent extends PicklistFormComponent implements OnInit {
+  selectedFilter: string;
   protected formArray: FormArray;
+
+  // Constants
+  readonly NONE_FILTER: string = 'None';
+  readonly DIFFICULTIES: string[] = [
+    Difficulty.TRIVIAL,
+    Difficulty.EASY,
+    Difficulty.NORMAL,
+    Difficulty.HARD,
+    Difficulty.INSANE
+  ];
+  readonly FILTERS: string[] = [
+    this.NONE_FILTER,
+    ...this.DIFFICULTIES
+  ];
 
   constructor(private controlContainer: ControlContainer, protected randomizerService: RandomizerService) {
     super(randomizerService);
+    this.selectedFilter = this.NONE_FILTER;
   }
 
   ngOnInit() {
     this.formArray = this.controlContainer.control.get('tricks') as FormArray;
     this.initialize();
+  }
+
+  setDifficulty(difficulty: string) {
+    // Reset set tricks in UI and form
+    this.formArray.clear();
+    this.items.available = this.items.available.concat(...this.items.selected);
+    this.items.selected = [];
+
+    if (difficulty !== this.NONE_FILTER) {
+      const fb = new FormBuilder();
+      const difficultyIndex = this.DIFFICULTIES.indexOf(difficulty);
+
+      for (let trickItem of (this.items.available as TrickItem[])) {
+        // If trick is within the difficulty threshold, select it
+        if (trickItem.difficulty && this.DIFFICULTIES.indexOf(trickItem.difficulty) <= difficultyIndex) {
+          this.formArray.push(fb.control(trickItem.value));
+          this.items.selected.push(trickItem);
+        }
+      }
+
+      // Filter any items in available that are in selected
+      if (this.items.selected.length > 0) {
+        this.items.available = this.items.available.filter(trickItem =>
+          this.items.selected.map(item => item.value).indexOf(trickItem.value) < 0);
+      }
+    }
   }
 
   protected initialize(): void {
@@ -27,10 +77,11 @@ export class TricksComponent extends PicklistFormComponent implements OnInit {
     for (let key of Object.keys(settings.tricks)) {
       const trickDetails = this.DETAILS[key];
 
-      const trick = {
+      const trick: TrickItem = {
         label: trickDetails ? trickDetails.name : key,
         value: key,
-        tooltip: trickDetails ? trickDetails.description : null
+        tooltip: trickDetails ? trickDetails.description : null,
+        difficulty: trickDetails ? trickDetails.difficulty : null
       };
 
       // If form contains value on init, push to selected tricks. Else, push to available tricks.
---
+++ src/electron/enums/primeRegion.ts
@@ -0,0 +1,7 @@
+export enum PrimeRegion {
+  TALLON_OVERWORLD = 'Tallon Overworld',
+  CHOZO_RUINS = 'Chozo Ruins',
+  MAGMOOR_CAVERNS = 'Magmoor Caverns',
+  PHENDRANA_DRIFTS = 'Phendrana Drifts',
+  PHAZON_MINES = 'Phazon Mines'
+}
