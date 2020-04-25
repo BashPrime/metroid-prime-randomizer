362f80da91b623cbd64e59c4aa5229509b9e5002
Evan Taylor <etaylor8086@gmail.com>
2020-04-24 21:19:15 -0700

    Update rules, exclude Chozo locations instead of prefilling them
    Nerfed chozo locations can now be filled with any junk item,
    not just Missile Expansions.

--- src/electron/models/prime/rules.ts
+++ src/electron/models/prime/rules.ts
@@ -21,38 +21,27 @@ export function setRules(world: PrimeWorld): void {
     locations.getLocationByKey(PrimeLocation.ARTIFACT_TEMPLE).setExcluded(true);
   }
 
-  // If the starting area is Chozo Transport North, automatically put Missile Launcher in Hive Totem to ensure the seed can be completed
-  if (world.getStartingArea().id === 1) {
-    const missileLauncher = primeItems[PrimeItem.MISSILE_LAUNCHER];
-    locations.getLocationByKey(PrimeLocation.HIVE_TOTEM).setItem(missileLauncher);
-    world.getItemPool().remove(missileLauncher);
-  }
-
   // Pre-fill Chozo Ruins randomly from 0-26 items to balance the logic
-  prefillChozoWithJunk(world);
+  nerfChozoLocations(world);
 }
 
-function prefillChozoWithJunk(world: PrimeWorld): void {
+function nerfChozoLocations(world: PrimeWorld): void {
   const rng = world.getRng();
-  const baseItemPool = world.getItemPool();
-  const junkItemPool = baseItemPool.shuffle(rng).filter((item: Item) => item.getPriority() === ItemPriority.EXTRA);
 
   const allChozoLocationNames = primeLocations.filter(location => location.region === PrimeRegion.CHOZO_RUINS)
     .map(location => location.name) as string[];
   const baseChozoLocations = world.getLocations().toArray().filter(location => {
     // Hive Totem needs to be open for most seeds, so don't include it in locations list
-    return allChozoLocationNames.includes(location.getName()) && location.getName() !== PrimeLocation.HIVE_TOTEM && !location.hasItem()
+    return allChozoLocationNames.includes(location.getName()) && location.getName() !== PrimeLocation.HIVE_TOTEM && !location.hasItem() && !location.isExcluded()
   });
-  // We are junk filling at most 75% (26) of the Chozo Ruins locations.
-  const numberOfLocationsToFill = Utilities.getRandomInt(0, Math.floor(allChozoLocationNames.length * 0.75), rng);
+  // We are junk filling at most 75% (26 maximum) of the Chozo Ruins locations.
+  const numberOfLocationsToNerf = Utilities.getRandomInt(0, Math.floor(allChozoLocationNames.length * 0.75), rng);
 
   // Get the Chozo locations we are going to fill
-  const junkChozoLocations = Utilities.randomArray(baseChozoLocations, numberOfLocationsToFill, rng);
+  const nerfedChozoLocations = Utilities.randomArray(baseChozoLocations, numberOfLocationsToNerf, rng);
 
-  // Fill the locations, and trim the item pool
-  for (const location of junkChozoLocations) {
-    const itemToFill = junkItemPool.pop();
-    location.setItem(itemToFill);
-    baseItemPool.remove(itemToFill);
+  // Exclude the locations from progression
+  for (const location of nerfedChozoLocations) {
+    location.setExcluded(true);
   }
 }
