7f94b5992980572a5f40efad831f5f2b234c9a0e
Evan Taylor <etaylor8086@gmail.com>
2020-08-11 14:56:59 -0700

    Don't persist starting items reference state

--- src/electron/models/prime/startingItems.ts
+++ src/electron/models/prime/startingItems.ts
@@ -4,7 +4,7 @@ import { Item } from '../item';
 import { PrimeWorld } from './world';
 import * as Utilities from '../../utilities';
 import { ItemOverrides } from './itemOverrides';
-import { ItemMap, mapToItemPool } from './itemPool';
+import { ItemMap } from './itemPool';
 
 interface StartingItems {
   [key: string]: {
@@ -17,45 +17,11 @@ interface StartingItems {
 }
 
 /**
- * Reference of items containing starting item information pertaining to the randomizer logic and the randomprime patcher.
- *
- * The un-excluded items in this list are intentionally ordered to match how the
- * randomprime patcher handles the starting items bitstring.
- */
-const items: StartingItems = {
-  [PrimeItem.SCAN_VISOR]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.MISSILE]: { bitWidth: 8, maximum: 0, randomPrimeOnly: true },
-  [PrimeItem.ENERGY_TANK]: { bitWidth: 4, maximum: 14 },
-  [PrimeItem.POWER_BOMB]: { bitWidth: 4, maximum: 1 },
-  [PrimeItem.WAVE_BEAM]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.ICE_BEAM]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.PLASMA_BEAM]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.CHARGE_BEAM]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.MORPH_BALL]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.MORPH_BALL_BOMB]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.SPIDER_BALL]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.BOOST_BALL]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.VARIA_SUIT]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.GRAVITY_SUIT]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.PHAZON_SUIT]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.THERMAL_VISOR]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.XRAY_VISOR]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.SPACE_JUMP_BOOTS]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.GRAPPLE_BEAM]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.SUPER_MISSILE]: { bitWidth: 1, maximum: 1 },
-  [PrimeItem.WAVEBUSTER]: { bitWidth: 1, maximum: 1, excludeFromRandomStartingItems: true },
-  [PrimeItem.ICE_SPREADER]: { bitWidth: 1, maximum: 1, excludeFromRandomStartingItems: true },
-  [PrimeItem.FLAMETHROWER]: { bitWidth: 1, maximum: 1, excludeFromRandomStartingItems: true },
-  [PrimeItem.MISSILE_LAUNCHER]: { bitWidth: 0, maximum: 1, excludeFromRandomPrime: true },
-  [PrimeItem.MISSILE_EXPANSION]: { bitWidth: 0, maximum: 49, excludeFromRandomPrime: true },
-  [PrimeItem.POWER_BOMB_EXPANSION]: { bitWidth: 0, maximum: 4, excludeFromRandomPrime: true }
-};
-
-/**
  * Sets the starting items for a world, based on the random starting settings and overrides.
  * @param world The world to set the starting items for.
  */
 export function setStartingItems(world: PrimeWorld): void {
+  const items: StartingItems = getItemsReference();
   const startingItems: ItemMap = getStartingItemsMap();
   const settings = world.getSettings();
   const rng = world.getRng();
@@ -128,6 +94,8 @@ export function toRandomprimeFormat(startingItems: Item[]): number {
   // Bitstring representing the number of items
   let itemBitString = '';
 
+  const items: StartingItems = getItemsReference();
+
   // Iterate through all un-excluded items (excluded items are not useful for building randomprime string)
   for (let [key, info] of Object.entries(items).filter(([key, info]) => !info.excludeFromRandomPrime)) {
     let value: number;
@@ -166,10 +134,48 @@ export function toRandomprimeFormat(startingItems: Item[]): number {
 }
 
 /**
+ * Returns a reference of items containing starting item information pertaining to the randomizer logic and the randomprime patcher.
+ *
+ * The un-excluded items in this list are intentionally ordered to match how the
+ * randomprime patcher handles the starting items bitstring.
+ */
+function getItemsReference(): StartingItems {
+  return {
+    [PrimeItem.SCAN_VISOR]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.MISSILE]: { bitWidth: 8, maximum: 0, randomPrimeOnly: true },
+    [PrimeItem.ENERGY_TANK]: { bitWidth: 4, maximum: 14 },
+    [PrimeItem.POWER_BOMB]: { bitWidth: 4, maximum: 1 },
+    [PrimeItem.WAVE_BEAM]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.ICE_BEAM]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.PLASMA_BEAM]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.CHARGE_BEAM]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.MORPH_BALL]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.MORPH_BALL_BOMB]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.SPIDER_BALL]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.BOOST_BALL]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.VARIA_SUIT]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.GRAVITY_SUIT]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.PHAZON_SUIT]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.THERMAL_VISOR]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.XRAY_VISOR]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.SPACE_JUMP_BOOTS]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.GRAPPLE_BEAM]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.SUPER_MISSILE]: { bitWidth: 1, maximum: 1 },
+    [PrimeItem.WAVEBUSTER]: { bitWidth: 1, maximum: 1, excludeFromRandomStartingItems: true },
+    [PrimeItem.ICE_SPREADER]: { bitWidth: 1, maximum: 1, excludeFromRandomStartingItems: true },
+    [PrimeItem.FLAMETHROWER]: { bitWidth: 1, maximum: 1, excludeFromRandomStartingItems: true },
+    [PrimeItem.MISSILE_LAUNCHER]: { bitWidth: 0, maximum: 1, excludeFromRandomPrime: true },
+    [PrimeItem.MISSILE_EXPANSION]: { bitWidth: 0, maximum: 49, excludeFromRandomPrime: true },
+    [PrimeItem.POWER_BOMB_EXPANSION]: { bitWidth: 0, maximum: 4, excludeFromRandomPrime: true }
+  };
+}
+
+/**
  * Returns an item map with each item set to 0.
  */
 function getStartingItemsMap(): ItemMap {
   const map: ItemMap = {};
+  const items: StartingItems = getItemsReference();
 
   // Build map out of non-randomprime-only items
   for (let [key, info] of Object.entries(items).filter(([key, info]) => !info.randomPrimeOnly)) {
