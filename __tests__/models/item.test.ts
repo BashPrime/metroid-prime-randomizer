e786a071815db7be283528c165d974e381bc22ed
BashPrime <5769306+BashPrime@users.noreply.github.com>
2021-01-05 21:26:00 -0800

    jest works!

--- __tests__/models/item.test.ts
+++ __tests__/models/item.test.ts
@@ -2,6 +2,8 @@ import { Item } from '../../src/electron/models/item';
 
 describe('Item', () => {
   it('constructs without error', () => {
-    expect(new Item('Test Item', 'test type', 0)).not.toThrow();
+    expect(() => {
+      new Item('Test Item', 'test type', 0);
+    }).not.toThrow();
   });
 });
